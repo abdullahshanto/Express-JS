b4bddd60874954d47f3c2ee20e290e4a3df3e501
AbdullahShanto <abdurrahman7328@gmail.com>
2026-07-28 11:12:54 +0600

    contact-page

--- basic.js
+++ basic.js
@@ -5,13 +5,22 @@ const port=3000;
 //dummy middleware....
 
 app.use((req,res,next)=>{
-  console.log("first middleware..")
+  console.log("first middleware.." , req.path , req.method)
   next();
 })
 
 app.use((req,res,next)=>{
-  console.log("2nd middleware..");
+  console.log("2nd middleware.." ,req.path ,req.method);
   next();
+})
+app.use("/contact-page",(req,res,next)=>{
+  console.log("3rd middleware..");
+  res.send(`
+    <form method="POST" action="/contact-page">
+      <input type="text" name="name" placeholder="Your name" />
+      <button type="submit">Submit</button>
+    </form>
+  `)
 })
 
 app.get('/' ,(req,res)=> {

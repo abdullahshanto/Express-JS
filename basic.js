07a09b5dbf7e9da3e26154a45c1e117272812007
AbdullahShanto <abdurrahman7328@gmail.com>
2026-07-28 11:00:25 +0600

    added middleware

--- basic.js
+++ basic.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const app= express();
+const port=3000;
 
-const port = 3000;
+//dummy middleware....
+
+app.use((req,res,next)=>{
+  console.log("first middleware..")
+  next();
+})
+
+app.use((req,res,next)=>{
+  console.log("2nd middleware..");
+  next();
+})
 
 app.get('/' ,(req,res)=> {
   res.send('hello world');
 })
+
+
 
 app.listen(port , ()=>{
   console.log('listening.......: http://localhost:3000')

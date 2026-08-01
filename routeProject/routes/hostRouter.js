890f5279dff3f49b760d01f983739767cdec8deb
AbdullahShanto <abdurrahman7328@gmail.com>
2026-08-01 13:43:31 +0600

    updated

--- routeProject/routes/hostRouter.js
+++ routeProject/routes/hostRouter.js
@@ -1,18 +1,9 @@
-const path = require('path');
-const express= require('express');
-const Hostrouter= express.Router();
+const path = require("path");
+const express = require("express");
+const Hostrouter = express.Router();
 
-const rootDir=require('../utils/pathUtils')
+const rootDir = require("../utils/pathUtils");
 
-Hostrouter.get("/add-home" , (req,res,next)=>{
-  res.sendFile(path.join(rootDir, 'views' , 'add-home.html'));
-})
-
-Hostrouter.post("/add-home" ,(req,res,next)=>{
-  res.sendFile(path.join(rootDir,'views','homeAdded.html'))
-})
-
-module.exports = Hostrouter;
-
-
-
+Hostrouter.get("/add-home", (req, res, next) => {
+  res.sendFile(path.join(rootDir, "views", "add-home.html"));
+});
